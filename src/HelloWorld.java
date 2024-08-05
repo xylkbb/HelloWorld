7724acd63925f7845761c11071f5d21588937d14
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-05 12:52:48 -0400

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello");
+        System.out.print("Software");
+        System.out.print("Development");
+
+    }
+
 }
+
+
